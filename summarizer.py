1b719afeb8ef39ce96625137f6acf5d10017e9dc
AliTalib123 <143446082+AliTalib123@users.noreply.github.com>
2026-04-22 18:14:02 +0300

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -15,7 +15,7 @@ def load_models():
         model="openai/whisper-base"
     )
     tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
-    model =BartForConditionalGeneration.from_pretrained("facebook/bart-base").to(device)
+    model =BartForConditionalGeneration.from_pretrained("facebook/bart-base")
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
     return asr_pipe, tokenizer, model, device
